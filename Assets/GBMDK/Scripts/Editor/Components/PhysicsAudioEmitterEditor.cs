27945ef431a349a2637e0957d8051a2de551c92b
TheUltimateNuke <nuke.public@gmail.com>
2025-10-17 16:37:36 -0400

    refactor: change MenuItem path for creation of PhysicsAudioData

--- Assets/GBMDK/Scripts/Editor/Components/PhysicsAudioEmitterEditor.cs
+++ Assets/GBMDK/Scripts/Editor/Components/PhysicsAudioEmitterEditor.cs
@@ -5,7 +5,7 @@ namespace GBMDK.Editor.Components
     [CustomEditor(typeof(PhysicAudioEmitter))]
     public class PhysicsAudioEmitterEditor : UnityEditor.Editor
     {
-        [MenuItem("Assets/Create/GBMDK/Physics Audio Data")]
+        [MenuItem("Assets/Create/Gang Beasts/Data/Physics Audio Data")]
         private static void CreateData()
         {
             Common.CreateAndSaveScriptableObject<PhysicsAudioData>();
