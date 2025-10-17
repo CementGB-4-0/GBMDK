823bea2d9acfe6a3171f7995d93bbe40e590d973
TheUltimateNuke <nuke.public@gmail.com>
2025-10-17 16:32:09 -0400

    refactor: select scriptableObject created via Common.cs

--- Assets/GBMDK/Scripts/Editor/Common.cs
+++ Assets/GBMDK/Scripts/Editor/Common.cs
@@ -23,6 +23,7 @@ namespace GBMDK.Editor
             AssetDatabase.CreateAsset(scriptableObject, GetCurrentSelectedAssetPath() + $"/New {typeof(T)}.asset");
             EditorUtility.SetDirty(scriptableObject);
             Undo.RecordObject(scriptableObject, "CreateAndSaveScriptableObject");
+            Selection.activeObject = scriptableObject;
 
             return scriptableObject;
         }
