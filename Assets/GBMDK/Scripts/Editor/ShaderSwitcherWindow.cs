50f69c9c9e69b7d098ca029c35a77b8a92f548ad
TheUltimateNuke <nuke.public@gmail.com>
2025-12-23 13:06:56 -0500

    feat: have ShaderSwitcher optionally take a name instead of a direct shader ref

--- Assets/GBMDK/Scripts/Editor/ShaderSwitcherWindow.cs
+++ Assets/GBMDK/Scripts/Editor/ShaderSwitcherWindow.cs
@@ -7,15 +7,33 @@ namespace GBMDK.Editor
     {
         private Shader _replaceWith;
         private Shader _toReplace;
+        private string _toReplaceName;
 
         private void OnGUI()
         {
-            _toReplace = (Shader)EditorGUILayout.ObjectField("Shader to Replace", _toReplace, typeof(Shader), false);
+            EditorGUILayout.BeginVertical();
+            var toReplaceNameShader = Shader.Find(_toReplaceName);
+            if (toReplaceNameShader != null)
+            {
+                EditorGUILayout.LabelField($"Replace using name: {_toReplaceName} (VALID)",
+                    EditorStyles.centeredGreyMiniLabel);
+                _toReplace = toReplaceNameShader;
+            }
+            else
+            {
+                _toReplace =
+                    (Shader)EditorGUILayout.ObjectField("Shader to Replace", _toReplace, typeof(Shader), false);
+            }
+
+            EditorGUILayout.LabelField("----- Optional -----", EditorStyles.boldLabel);
+            _toReplaceName = EditorGUILayout.TextField("Shader Name to Replace", _toReplaceName);
+            EditorGUILayout.LabelField("----------", EditorStyles.boldLabel);
             _replaceWith =
                 (Shader)EditorGUILayout.ObjectField("Replacement Shader", _replaceWith, typeof(Shader), false);
 
             if (GUILayout.Button("Replace!"))
                 ReplaceShaders();
+            EditorGUILayout.EndVertical();
         }
 
         [MenuItem("Tools/GBMDK/Shader Switcher")]
